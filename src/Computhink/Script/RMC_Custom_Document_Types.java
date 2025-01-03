0606d4f083226547e23993ad4a9517ab9fe65fad
Vidya3011 <86242849+Vidya3011@users.noreply.github.com>
2025-01-03 19:54:03 +0530

    Update RMC_Custom_Document_Types.java

--- src/Computhink/Script/RMC_Custom_Document_Types.java
+++ src/Computhink/Script/RMC_Custom_Document_Types.java
@@ -20,11 +20,11 @@ public class RMC_Custom_Document_Types  extends BaseClass {
 
 		log.info("Chrome Browser Launched");
 		Reporter.log("Chrome Browser launched Successfully...");
-<<<<<<< HEAD
+
 		launch47Url();
-=======
+
 		launchUrl();
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 		// launchLocalUrl();
 		Reporter.log("Contentverse URL launched successfully...");
 		log.info("Contentverse URL launched successfully...");
