256c96328390a22fec215f6463e54bb99f3a1c23
Vidya3011 <86242849+Vidya3011@users.noreply.github.com>
2025-01-03 19:04:47 +0530

    Update AnnotationFunctionality.java

--- src/Computhink/Script/AnnotationFunctionality.java
+++ src/Computhink/Script/AnnotationFunctionality.java
@@ -27,11 +27,11 @@ public class AnnotationFunctionality extends Computhink.Generic.BaseClass {
 	
 		log.info("Chrome Browser Launched");
 		Reporter.log("Chrome Browser launched Successfully...");
-<<<<<<< HEAD
+
 	launch47Url();
-=======
+
 		launchUrl();
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 		//launchUrl();
 		// launchLocalUrl();
 		Reporter.log("Contentverse URL launched successfully...");
@@ -55,7 +55,7 @@ public class AnnotationFunctionality extends Computhink.Generic.BaseClass {
 	
 
 	}
-<<<<<<< HEAD
+
 	@Test(priority = 3)
 	public void TC_B_CreateDocument() throws Exception {
 		AnnotaionsPom an = new AnnotaionsPom();
@@ -65,35 +65,35 @@ public class AnnotationFunctionality extends Computhink.Generic.BaseClass {
 
 	@Test(priority = 4)
 	public void TC_c_Redaction_UnlockRedaction() throws Exception {
-=======
+
 
 	@Test(priority = 3)
 	public void TC_B_Redaction_UnlockRedaction() throws Exception {
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 		AnnotaionsPom an = new AnnotaionsPom();
 		an.RedactionAnnotation();
 
 		log.info("Redaction added and Unlocked redaction successful");
 	}
 
-<<<<<<< HEAD
+
 	@Test(priority = 5)
 	public void TC_D_HighLight_Annotation() throws Exception {
-=======
+
 	@Test(priority = 4)
 	public void TC_C_HighLight_Annotation() throws Exception {
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 		AnnotaionsPom an = new AnnotaionsPom();
 		an.HighLighFunction();
 		log.info("Page HighLighted successful");
 
 	}
 
-<<<<<<< HEAD
+
 	@Test(priority = 6)
-=======
+
 	@Test(priority = 5)
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 	public void TC_E_StikyNote_Annotation() throws Exception {
 		AnnotaionsPom an = new AnnotaionsPom();
 		an.StickyNoteAnnotation();
@@ -101,33 +101,33 @@ public class AnnotationFunctionality extends Computhink.Generic.BaseClass {
 		Thread.sleep(3000);
 	}
 
-<<<<<<< HEAD
+
 	@Test(priority = 7)
-=======
+
 	@Test(priority = 6)
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 	public void TC_F_RubberStampAnnotation() throws Exception {
 		AnnotaionsPom an = new AnnotaionsPom();
 		an.RubberStampAnnotation();
 		log.info("RubberStamp Annotation Added Successful");
 	}
 
-<<<<<<< HEAD
+
 	@Test(priority = 8)
-=======
+
 	@Test(priority = 7)
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 	public void TC_G_Rectangle_Annotation() throws Exception {
 		AnnotaionsPom an = new AnnotaionsPom();
 		an.rectangleAnnotation();
 		log.info("Rectangle annotation added successful");
 	}
 
-<<<<<<< HEAD
+
 	@Test(priority = 9)
-=======
+
 	@Test(priority = 8)
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 	public void TC_H_StampAnnotation() throws Exception {
 		AnnotaionsPom an = new AnnotaionsPom();
 		an.StampAnnotation();
@@ -135,11 +135,11 @@ public class AnnotationFunctionality extends Computhink.Generic.BaseClass {
 
 	}
 
-<<<<<<< HEAD
+
 	@Test(priority = 10)
-=======
+
 	@Test(priority = 9)
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 	public void TC_I_LineAnnotation_Ellipse_Annotation() throws Exception {
 		AnnotaionsPom an = new AnnotaionsPom();
 		an.LineAnnotation();
@@ -150,11 +150,11 @@ public class AnnotationFunctionality extends Computhink.Generic.BaseClass {
 
 	}
 
-<<<<<<< HEAD
+
 	@Test(priority = 11)
-=======
+
 	@Test(priority = 10)
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 	public void TC_J_Signature_Annotation() throws Exception {
 		AnnotaionsPom an = new AnnotaionsPom();
 		an.signatureAnnotation();
@@ -162,11 +162,11 @@ public class AnnotationFunctionality extends Computhink.Generic.BaseClass {
 		log.info("Signature added successfully");
 	}
 
-<<<<<<< HEAD
+
 	@Test(priority = 12)
-=======
+
 	@Test(priority = 11)
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 	public void TC_K_Polygon() throws Exception {
 		AnnotaionsPom an = new AnnotaionsPom();
 		log.info("polygon added successfully");
