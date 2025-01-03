9ce9ef0eaaef29e6275886992095b0968eedd89b
Vidya3011 <86242849+Vidya3011@users.noreply.github.com>
2025-01-03 19:38:31 +0530

    Update Login_Validation.java

--- src/Computhink/Script/Login_Validation.java
+++ src/Computhink/Script/Login_Validation.java
@@ -1,10 +1,10 @@
 package Computhink.Script;
 
-<<<<<<< HEAD
+
 //Dipak Automation Coading
 
 import org.openqa.selenium.WindowType;
-=======
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 
@@ -13,27 +13,27 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 import org.testng.Reporter;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-<<<<<<< HEAD
+
 import Computhink.Pom.Language;
-=======
+
 import Computhink.Pom.Login;
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 
 public class Login_Validation extends Computhink.Generic.BaseClass {
 
 	@BeforeClass
 	public void Launch_Browser() throws Exception {
 
-<<<<<<< HEAD
+
 		loadBrowser("edge");
-=======
+
 		loadBrowser("Chrome");
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 		launchUrl();
 		Reporter.log("CVS URL started Successfully", true);
 	}
@@ -46,13 +46,13 @@ public class Login_Validation extends Computhink.Generic.BaseClass {
 
 	}
 
-<<<<<<< HEAD
+
 	@Test(priority = 2)
 	public void TC_02_Verifying_Login_EWA_with_blank_Username() throws Exception {
 
 		Language pojo = new Language();
 		Thread.sleep(2000);
-=======
+
 	@Test(priority = 1)
 	public void TC_01_Verifying_No_button_of_Session_for_user_is_already_active_dialog_box() throws Exception {
 
@@ -72,18 +72,18 @@ public class Login_Validation extends Computhink.Generic.BaseClass {
 
 		Login pojo = new Login();
 		Thread.sleep(4000);
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 		pojo.Verifying_Login_EWA_with_blank_Username();
 	}
 
 	@Test(priority = 3)
 	public void TC_03_Verifying_Login_EWA_with_blank_Password() throws Exception {
 
-<<<<<<< HEAD
+
 		Language pojo = new Language();
-=======
+
 		Login pojo = new Login();
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 		pojo.Verifying_Login_EWA_with_blank_Password();
 
 	}
@@ -91,11 +91,11 @@ public class Login_Validation extends Computhink.Generic.BaseClass {
 	@Test(priority = 4)
 	public void TC_04_Verifying_Login_EWA_without_Selecting_Roomname() throws Exception {
 
-<<<<<<< HEAD
+
 		Language pojo = new Language();
-=======
+
 		Login pojo = new Login();
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 		pojo.Verifying_Login_EWA_without_Selecting_Roomname();
 
 	}
@@ -103,11 +103,11 @@ public class Login_Validation extends Computhink.Generic.BaseClass {
 	@Test(priority = 5)
 	public void TC_05_Verifying_Login_EWA_with_Invalid_Username() throws Exception {
 
-<<<<<<< HEAD
+
 		Language pojo = new Language();
-=======
+
 		Login pojo = new Login();
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 		pojo.Verifying_Login_EWA_with_Invalid_Username();
 
 	}
@@ -115,7 +115,7 @@ public class Login_Validation extends Computhink.Generic.BaseClass {
 	@Test(priority = 6)
 	public void TC_06_Verifying_Login_EWA_with_Invalid_Password() throws Exception {
 
-<<<<<<< HEAD
+
 		Language pojo = new Language();
 		pojo.Verifying_Login_EWA_with_Invalid_Password();
 
@@ -127,7 +127,7 @@ public class Login_Validation extends Computhink.Generic.BaseClass {
 		pojo.Verifying_Yes_button_of_Session_for_user_is_already_active_dialog_box();
 	}
 
-=======
+
 		Login pojo = new Login();
 		pojo.Verifying_Login_EWA_with_Invalid_Password();
 		
@@ -138,5 +138,5 @@ public class Login_Validation extends Computhink.Generic.BaseClass {
 		Login pojo = new Login();
 		pojo.Verifying_Yes_button_of_Session_for_user_is_already_active_dialog_box();
 	}
->>>>>>> origin/Dipak-Pandurang-Gawali
+
 }
